0002fdbeccab29d8681635dcaa79c23365f59ed9
ecwright3 <ecwright3@gmail.com>
2018-02-24 19:01:20 -0500

    prep test_modules.py

---
+++ tests/test_modules.py
@@ -0,0 +1,60 @@
+import unittest
+from ..casey import modules
+
+
+# Cloudflare Tests
+
+
+
+# Digital Ocean Tests
+
+
+
+# Downloader Tests
+
+
+
+# Dropbox Tests
+
+
+
+# Freenom Tests
+
+
+
+
+# Gcalendar Tests
+
+
+
+# Github Tests
+
+
+
+# Ifttt Tests
+
+
+
+# Mailgun Tests
+
+
+
+# Slack Tests
+
+
+
+# Traffic Tests 
+
+
+
+
+# Twitter Tests
+
+
+
+
+# Weather Tests
+
+
+
+
