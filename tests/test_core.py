f5370289500b2006856be0838e927173a447d20e
ecwright3 <ecwright3@gmail.com>
2018-02-24 19:05:03 -0500

    changed assertEquals to assertEqual in test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,9 +4,9 @@ from ..casey import core
 class TestCore(unittest.TestCase):
     def test_read_instructions(self):
         message = "test message"
-        self.assertEquals(core.read_instruction("test message"), message)
-        self.assertEquals(core.read_instruction(1), 1)
-        self.assertEquals(core.read_instruction(True), True)
+        self.assertEqual(core.read_instruction("test message"), message)
+        self.assertEqual(core.read_instruction(1), 1)
+        self.assertEqual(core.read_instruction(True), True)
 
     def test_values(self):
         self.assertRaises(ValueError, core.read_instruction, None)
